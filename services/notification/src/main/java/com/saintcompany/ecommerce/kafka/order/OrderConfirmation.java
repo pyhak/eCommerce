96ca6a60754d8c210d2da6cd905951aa4b1aa844
Madis Mätlik <madis.matlik@cgi.com>
2024-09-17 12:25:43 +0300

    added notification-server configurations

--- services/notification/src/main/java/com/saintcompany/ecommerce/kafka/order/OrderConfirmation.java
+++ services/notification/src/main/java/com/saintcompany/ecommerce/kafka/order/OrderConfirmation.java
@@ -1,6 +1,9 @@
 package com.saintcompany.ecommerce.kafka.order;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import com.saintcompany.ecommerce.kafka.payment.PaymentMethod;
 
 public record OrderConfirmation(
     String orderReference,
